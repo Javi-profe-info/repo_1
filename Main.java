6eb87b412038e4be0329e5fe22abc5eb049112d3
Droide KO <you@example.com>
2026-09-21 09:37:30 +0200

    Commit desde VS Code

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Learning Git");
+        System.out.println("Now with VS Code");
     }
 }
